69c7020854862bb31d682183f3bdd8775fefb7b5
Копылов Сергей <asu-dev005@yandex.ru>
2023-01-24 21:23:16 +0500

    Add C# Project

---
+++ Binding_Example_1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Binding_Example_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int SrvStarted = 0;
+        public int ClientsConnected = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Thread t = new(Thread_Cyclic_Update);
+            t.Start();
+        }
+
+        private void Button_Start_Click(object sender, RoutedEventArgs e)
+        {
+            var bindExpr_StatusMsg = BindingOperations.GetBindingExpression(StatusMsg, Label.ContentProperty);
+            var sourceData_StatusMsg = (Message)bindExpr_StatusMsg.DataItem;
+
+            sourceData_StatusMsg.MessageText = "Server started";
+
+            SrvStarted = 1;
+        }
+
+        private void Button_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            var bindingExpression = BindingOperations.GetBindingExpression(StatusMsg, Label.ContentProperty);
+            var sourceData = (Message)bindingExpression.DataItem;
+
+            sourceData.MessageText = "SERVER STOPPED!";
+
+            SrvStarted = 0;
+        }
+
+        public void Thread_Cyclic_Update()
+        {
+            var bindExpr_ClientCnt = BindingOperations.GetBindingExpression(ClientsCntMsg, Label.ContentProperty);
+            var sourceDataClientCnt = (Message)bindExpr_ClientCnt.DataItem;
+
+            while (true)
+            {
+                if (SrvStarted == 1)
+                {
+                    // Количество подключенных клиентов
+                    ClientsConnected++;
+                }
+                else
+                {
+                    ClientsConnected = 0;
+                }
+                sourceDataClientCnt.ClientsCnt = ClientsConnected.ToString();
+                Thread.Sleep(250);
+            }
+        }
+
+    }
+}
---
+++ Binding_Example_1/Message.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+
+namespace Binding_Example_1
+{
+    public class Message : INotifyPropertyChanged
+    {
+        private string _message;
+
+        public Message()
+        {
+        }
+
+        public Message(string message)
+        {
+            _message = message;
+        }
+
+        public string MessageText
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged(nameof(MessageText));
+            }
+        }
+        
+
+        private string _clientsCnt;
+        public string ClientsCnt
+        {
+            get { return _clientsCnt; }
+            set
+            {
+                _clientsCnt = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged(nameof(ClientsCnt));
+            }
+        }
+
+
+        private string _txtBox1;
+        public string TxtBox1
+        {
+            get { return _txtBox1; }
+            set
+            {
+                _txtBox1 = value;
+                _txtBox2 = _txtBox1;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged(nameof(TxtBox1));
+                OnPropertyChanged(nameof(TxtBox2));
+            }
+        }
+
+
+        private string _txtBox2;
+        public string TxtBox2
+        {
+            get { return _txtBox2; }
+            set
+            {
+                _txtBox2 = value;
+                _txtBox1 = _txtBox2;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged(nameof(TxtBox2));
+                OnPropertyChanged(nameof(TxtBox1));
+            }
+        }
+
+
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
